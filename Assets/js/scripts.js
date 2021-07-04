16773a10f7e21b6919fbbfe3b2692eadaba89400
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-07-04 19:26:06 +0530

    added carousel with 4 pics, also almost-mobile-6

---
+++ Assets/js/scripts.js
@@ -0,0 +1,42 @@
+$(".show").hide();
+
+$(".nav-link").on("click", function() {
+    $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+});
+
+function hideSoc(){
+    $(".soc").hide();
+    $(".show").show();
+    $(".hide").hide();
+};
+
+function showSoc(){
+    $(".soc").show();
+    $(".hide").show();
+    $(".show").hide();
+};
+
+$("#aboutBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 200);
+});
+
+$("#homeBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 200);
+});
+
+$("#contactBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 200);
+});
+
+$("#queryBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 200);
+});
